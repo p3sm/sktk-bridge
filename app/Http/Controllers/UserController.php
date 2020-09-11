d86872245be36159c705ffc7dad4e32eea7ff641
maulanardy <38031045+maulanardy@users.noreply.github.com>
2020-09-11 23:17:26 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,8 +45,8 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $find = User::where("username", $request->get('username'))->first();
-        dd($find);
-        if($find != null){
+
+        if($find){
             return redirect('/users/create')->with('error', 'User sudah ada');
         }
 
